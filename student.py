209272f8a75f6e43207be0ea0ec0c36d7fd44707
ariel18-meet <arielhito6901@gmail.com>
2016-12-15 19:51:17 +0200

    made a class to describe a student

---
+++ student.py
@@ -0,0 +1,16 @@
+class Student:
+    def __init__ (self, name, hometown, age, height,icflavor):
+        self.name=name
+        self.hometown= hometown
+        self.age= age
+        self.height=height
+        self.icflavor= icflavor
+
+    def print_summary(self):
+        print(self.name
+        self.hometown
+        self.age
+        self.height
+        self.icflavor)
+        
+    
